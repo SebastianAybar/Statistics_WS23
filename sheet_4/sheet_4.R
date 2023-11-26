592aec07ba034897652ac4f4cd6b2625c9d48737
Sebastian Aybar <38281694+SebiAybar@users.noreply.github.com>
2023-11-26 19:32:43 +0100

    finish sheet 4 task 1.1.

--- sheet_4/sheet_4.R
+++ sheet_4/sheet_4.R
@@ -1,5 +1,4 @@
 #sheet 4
-
 library("tidyverse")
 library("tidyr")
 library("dplyr")
@@ -12,5 +11,31 @@ plot(x = x, y = y,
      xlim = c(0,10),
      ylim = c(0,10))
 
+#(a) covariance
 covariance <- cov(x,y)
-print(covariance)
+#(b)  coefficient of correlation
+sd_x <- sd(x)
+sd_y <- sd(y)
+var_x <- var(x)
+var_y <- var(y)
+coef_cor <- covariance/(sd_x * sd_y)
+#Funktion aus R
+coef_cor <- cor(x,y)
+#regression line: criterion variable Y and predictor variable X
+linear_regression <- lm(y ~ x)
+summary(linear_regression)
+plot(x = x, y = y,
+     xlim = c(0,10),
+     ylim = c(0,10))
+abline(linear_regression)
+#regression line: criterion variable X and predictor variable Y
+linear_regression <- lm(x ~ y)
+summary(linear_regression)
+plot(x = y, y = x,
+     xlim = c(0,10),
+     ylim = c(0,10))
+abline(linear_regression)
+
+
+
+
