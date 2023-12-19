aff7516d505c24c4e79abe43aa9ab97f9747ae36
Sebastian Aybar <38281694+SebiAybar@users.noreply.github.com>
2023-12-19 09:11:39 +0100

    update sheet 6

--- sheet_6/sheet_6.R
+++ sheet_6/sheet_6.R
@@ -4,7 +4,7 @@ library("tidyr")
 library("dplyr")
 library("ggplot2")
 
-#Aufgabe 1.1.a
+#Aufgabe 1.1.(a)
 #Consider an urn with 100 balls, where are 30 balls of them are red. 
 #20 balls are randomly drawn and let X be the number of red drawn balls.
 total_balls <- 100
@@ -12,9 +12,33 @@ red_balls <- 30
 drawn_balls <- 20
 possible_X <- 0:drawn_balls
 probs <- dhyper(x = possible_X, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+#(b) Plot.
 barplot(probs, 
         names.arg = possible_X, 
         xlab = "Number of Red Balls Drawn", 
         ylab = "Probability", 
         main = "Hypergeometric Distribution")
-print(probs)
+#Die Wahrscheinlichkeit ist für k=6 am höchsten!
+
+#(c)Generate a sample of size 20 of values of X.
+k_sample <- sample(possible_X, size = 20, replace = FALSE)
+sample <- dhyper(k_sample, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+barplot(sample)
+
+#(e)
+q1 <- qhyper(0.25, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+q2 <- qhyper(0.50, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+q3 <- qhyper(0.75, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+
+#
+random_sample <- rhyper(20, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+print(random_sample)
+
+#(d)
+a <- phyper(15, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+b <- phyper(5-1, m = red_balls, n = total_balls - red_balls, k = drawn_balls)
+x <- a-b
+
+
+
+
